32546f411bfe0ddd797d0b5431078c842fc0244b
Tony <tony@gmail.com>
2017-11-02 23:26:28 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -0,0 +1,18 @@
+package com.coolweather.android;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lenovo on 2017/11/2.
+ */
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
--- app/src/main/java/com/coolweather/android/db/County.java
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -6,7 +6,7 @@ import org.litepal.crud.DataSupport;
  * Created by lenovo on 2017/10/30.
  */
 
-public class Country extends DataSupport {
+public class County extends DataSupport {
     private int id;
 
     private String countyName;
